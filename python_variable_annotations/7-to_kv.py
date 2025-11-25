116b25425e4f1476791118294e9e8b35f0551973
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-25 11:43:57 +0100

    task 7 with a fuller function description

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,5 +6,13 @@ from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
-    """"Return a tuple with the string k and the square of v as a float."""
+    """Return a tuple with the string k and the square of v as a float.
+
+        Args:
+            k: A string representing the key.
+            v: An integer or float representing the value.
+
+        Returns:
+            A tuple where the first element is the key and the second is the square of v as a float.
+        """
     return (k, float(v ** 2))
