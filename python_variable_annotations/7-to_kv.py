b08831f88e4d668beaf1a620ea60a06120d7c3e6
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-25 13:42:26 +0100

    task 7 + 9; pycodestyled

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-"""This module provides a function to convert a key and a value to a key-value tuple.
+"""This module provides a function to convert a key and a value to a
+key-value tuple.
 
 The function to_kv takes a string key and a numeric value (int or float),
 and returns a tuple with the key and the square of the value as a float.
 """
+
+
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with the string k and the square of v as a float."""
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-"""This module provides a function to return a list of tuples with elements and their lengths.
+"""This module provides a function to return a list of tuples with elements
+and their lengths.
 
-The function element_length takes an iterable of sequences (e.g., strings, lists, tuples)
-and returns a list of tuples, each containing an element and its length.
+The function element_length takes an iterable of sequences (e.g., strings,
+lists, tuples) and returns a list of tuples, each containing an element and
+its length.
 """
+
+
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples, each containing an element and its length."""
