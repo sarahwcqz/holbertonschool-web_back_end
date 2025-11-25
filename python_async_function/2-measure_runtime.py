bfb83f9b54092681af1463ed7fa8cfb5c95dc89c
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-25 15:46:40 +0100

    adding docstring to function of task 2 + 3

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,6 +9,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
+    """Fucntion that measures the total execution time for
+    wait_n(n, max_delay), and returns total_time / n."""
     start = time.time()
     await wait_n(n, max_delay)
     total_time = time.time() - start
