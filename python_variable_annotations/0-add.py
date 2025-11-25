cd9ead29c153cdcb7dd1494e20641ff87f87f52c
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-25 09:57:48 +0100

    task0 pycodestyled

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """module to learn how to use python's annotations"""
 
-def add(a : float, b :float) -> float:
-    return(a+b)
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers."""
+    return a + b
