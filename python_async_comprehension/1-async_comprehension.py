1795b45c14e742a7923e5a9511b552ac46ba9a7b
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-26 10:27:28 +0100

    task 1; async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to learn the async comprehension"""
+
+
+import random
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers using async comprehension over
+    async_generator."""
+    return [i async for i in async_generator()]
