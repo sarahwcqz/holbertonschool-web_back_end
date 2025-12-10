93106958e1270346d30cade17e4fe0877e39c0c2
sarahwcqz <sarah.wacquiez@gmail.com>
2025-12-10 12:45:08 +0100

    task 6 to 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,28 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        if (typeof brand !== "string") throw new TypeError("brand must be a string");
+        if (typeof motor !== "string") throw new TypeError("motor must be a string");
+        if (typeof color !== "string") throw new TypeError("color must be a string");
+
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    get brand() {
+        return this._brand;
+    }
+
+    get motor() {
+        return this._motor;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+
+    cloneCar() {
+        return new this.constructor(this._brand, this._motor, this._color);
+    }
+}
---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building{
+    constructor(sqft, floors){
+        super(sqft);
+        if (typeof floors !== "number") {
+            throw new TypeError("floors must be a number");
+        }
+        this._floors = floors
+    }
+
+    get floors() {
+        return this._floors;
+    }
+
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this.floors} floors`;
+    }
+}
