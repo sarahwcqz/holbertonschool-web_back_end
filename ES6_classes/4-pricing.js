0aa646ce5a4b753fa103bc9243782fbcd1f70ea7
sarahwcqz <sarah.wacquiez@gmail.com>
2025-12-10 12:50:22 +0100

    [fix] task 4 ; adding getters + setters

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -12,6 +12,20 @@ export default class Pricing{
         this._currency = currency;
     }
 
+    get amount() { return this._amount; }
+    set amount(newAmount) {
+        if (typeof newAmount !== "number") throw new TypeError("amount must be a number");
+        this._amount = newAmount;
+    }
+
+    get currency() { return this._currency; }
+    set currency(newCurrency) {
+        if (!newCurrency || typeof newCurrency !== "object" || !("code" in newCurrency) || !("name" in newCurrency)) {
+        throw new TypeError("currency must be a Currency object");
+        }
+        this._currency = newCurrency;
+    }
+
     displayFullPrice() {
         return `${this._amount} ${this._currency.name} (${this._currency.code})`;
     }
