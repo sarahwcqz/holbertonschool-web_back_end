98375e95f7bfd95b484a02dd93a24b6ccf676664
sarahwcqz <sarah.wacquiez@gmail.com>
2025-12-17 14:43:12 +0100

    [fix] task 6 : removing return

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,12 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName){
-      return Promise.allSettled([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ])
-  .then(results => results.map(result => {
-    return {
-      status: result.status,          // 'fulfilled' ou 'rejected'
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    };
-  }));
+  .then(results => results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason
+  })));
 }
