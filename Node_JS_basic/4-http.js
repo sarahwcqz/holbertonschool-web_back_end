080a91bb177ece45c8c7b9f5efb08fdfabdc3b97
sarahwcqz <sarah.wacquiez@gmail.com>
2026-01-05 18:51:11 +0100

    [fix] ESlint complying

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,10 @@
 const http = require('node:http');
 
-
 const app = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello Holberton School!');
-})
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
 
 app.listen(1245);
 
