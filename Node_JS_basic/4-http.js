85c82fcdf158c3afba87cc68d4c91391e2711040
sarahwcqz <sarah.wacquiez@gmail.com>
2026-01-05 18:22:33 +0100

    task 4 : Create a small HTTP server

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+
+const app = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello Holberton School!');
+})
+
+app.listen(1245);
+
+module.exports = app;
