4f8995ec99180f0b31126513bde0bcfd36d0ffe8
sarahwcqz <sarah.wacquiez@gmail.com>
2026-01-07 11:01:25 +0100

    [fix] task 7 : changing logic to do the same than in 5, using write instead of building an output var

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -10,37 +10,37 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   const dbFile = process.argv[2];
 
-  let output = 'This is the list of our students\n';
+  res.set('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
 
   fs.readFile(dbFile, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Cannot load the database');
+      res.status(500).end('Cannot load the database');
       return;
     }
 
     const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter((line) => line.trim() !== '');
-    output += `Number of students: ${students.length}\n`;
+    const students = lines.slice(1).filter(line => line.trim() !== '');
+
+    res.write(`Number of students: ${students.length}\n`);
 
     const fields = {};
 
-    students.forEach((line) => {
+    students.forEach(line => {
       const [firstname, , , field] = line.split(',');
       if (!fields[field]) fields[field] = [];
       fields[field].push(firstname);
     });
 
     const fieldNames = Object.keys(fields);
-    fieldNames.forEach((field, index) => {
-      output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
-      if (index !== fieldNames.length - 1) {
-        output += '\n';
-      }
+    fieldNames.forEach((field) => {
+      res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
     });
 
-    res.send(output);
+    res.end();
   });
 });
+
 
 app.listen(1245);
 
