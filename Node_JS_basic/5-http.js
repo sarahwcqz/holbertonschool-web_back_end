89e5ce2939542c167d6898a04408c4c7ac841b37
sarahwcqz <sarah.wacquiez@gmail.com>
2026-01-07 09:59:43 +0100

    [fix] task 5 : rewritting all logic for display of students

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,5 @@
 const http = require('node:http');
-const countStudents = require('./3-read_file_async');
-const db = process.argv[2];
+const fs = require('fs');
 
 const app = http.createServer((request, response) => {
     response.statusCode = 200;
@@ -9,11 +8,46 @@ const app = http.createServer((request, response) => {
     if (request.url === '/') {
         response.end('Hello Holberton School!');
     } else if (request.url === '/students') {
-        response.write('This is the list of our students');
+        response.write('This is the list of our students\n');
 
-        countStudents(db)
-            .then(data => response.end(data))
-            .catch(() => response.end('Cannot load the database'));
+        fs.readFile(process.argv[2], 'utf8', (err, data) => {
+            if (err) {
+                response.statusCode = 500;
+                response.end('Cannot load the database');
+                return;
+            }
+
+            const lines = data.trim().split('\n');
+            const numberOfStudents = lines.length - 1;
+            response.write(`Number of students: ${numberOfStudents}`);
+
+            // all students + removing header (ie first line)
+            const students = lines.slice(1);
+
+            const fields = {};
+
+            students.forEach((line) => {
+                // eslint-disable-next-line no-unused-vars
+                const [firstname, lastname, age, field] = line.split(',');
+                // if field not already in fields
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+                // adding name to field
+                fields[field].push(firstname);
+            });
+
+            // display msg for each field
+            for (const field in fields) {
+                if (Object.hasOwn(fields, field)) {
+                    const list = fields[field];
+                    response.write(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}\n`);
+                }
+            }
+            response.end();
+        });
+    } else {
+        response.end('Hello Holberton School!');
     }
 });
 
