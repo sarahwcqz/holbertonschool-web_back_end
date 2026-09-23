9b2a752b9675923ffcf658fcbdd31415c77a4272
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-09-23 15:33:02 +0200

    task 7 : return the basic value of header

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ auth = None
 if getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-else:
+elif getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication system class"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            return authorization_header.removeprefix("Basic ")
