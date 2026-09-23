7978cfd7c36a29cae98502030c112f557d9c97ed
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-09-23 14:00:39 +0200

    task 4 : defining authentication needs

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,15 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Tell whether the given path has to be authenticated."""
-        return False
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith("/"):
+            path = path + "/"
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Return the Authorization header carried by the request."""
