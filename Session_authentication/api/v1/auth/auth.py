95b76fa2a93b399c6aa2f337445317ee62cc876c
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-09-25 14:43:59 +0200

    task 4 : return the value of a cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -29,3 +30,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """Return the user the request is authenticated as."""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from the request"""
+        if request is None:
+            return None
+        cookie_name = getenv("SESSION_NAME")
+        return request.cookies.get(cookie_name)
