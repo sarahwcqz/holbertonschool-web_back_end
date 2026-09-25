e3a9ab5be85ce4217379861f0e5ca86f5dbb54b0
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-09-25 14:59:57 +0200

    task 5 : add a new url to list of auth required routes

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -53,10 +53,13 @@ def author():
         return
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.session_cookie(request) is None:
+            abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
         abort(403)
