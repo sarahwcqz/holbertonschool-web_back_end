715fda40ca5f690af7cab0ab18b5796a5938e624
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-09-25 14:00:00 +0200

    task 1 : create a session class

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -6,7 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
 
 
 app = Flask(__name__)
@@ -19,7 +19,9 @@ if getenv("AUTH_TYPE") == "basic_auth":
 elif getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-
+elif getenv("AUTH_TYPE") == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Session Auth module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session Auth system class"""
